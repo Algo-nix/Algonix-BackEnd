33466e22b19a8ed37c812b78a9213200192aa56f
ParkMini <parkteammini@gmail.com>
2024-10-02 20:06:31 +0900

    refactor: User 엔티티에 추가 필드 (역할, 레벨) 반영

---
+++ src/main/kotlin/com/algonix/model/Role.kt
@@ -0,0 +1,5 @@
+package com.algonix.model
+
+enum class Role {
+    USER, MANAGER, ADMIN
+}
--- src/main/kotlin/com/algonix/model/User.kt
+++ src/main/kotlin/com/algonix/model/User.kt
@@ -30,5 +30,10 @@ data class User(
     @NotEmpty(message = "소속 공개 여부를 선택해 주세요.")
     val visibility: String,  // "공개" 또는 "비공개"로 설정 가능
 
-    val statusMessage: String? = null  // 상태 메시지는 선택 사항
+    val statusMessage: String? = null,  // 상태 메시지는 선택 사항
+
+    @Enumerated(EnumType.STRING)
+    val role: Role = Role.USER,  // 기본값으로 일반 사용자 설정
+
+    var xp: Int = 0  // 사용자 경험치 초기값은 0
 )
